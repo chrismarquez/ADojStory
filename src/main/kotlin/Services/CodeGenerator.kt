375b3583c6d1e1d7db4b8c9358ae5b7e9562b883
Eduardo Merino <edu.merino996@gmail.com>
2018-11-15 21:47:00 -0600

    Add Enum Expression Type

--- src/main/kotlin/Services/CodeGenerator.kt
+++ src/main/kotlin/Services/CodeGenerator.kt
@@ -4,16 +4,24 @@ import Interfaces.ICodeGenerator
 import Models.Expression
 
 class CodeGenerator : ICodeGenerator {
+
+    private var fields: MutableList<String> = mutableListOf<String>()
+    private var variables: MutableList<String> = mutableListOf<String>()
+
+
     override fun genField(key: String, value: String): ICodeGenerator {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        var newfield = "$key : $value,"
+        this.fields.add(newfield)
+        this.variables.add(key)
+        return this
     }
 
     override fun genStatement(expression: Expression, varName: String?): ICodeGenerator {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return this
     }
 
     override fun genMethod(name: String, args: List<String>): ICodeGenerator {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return this
     }
 
     override fun buildObject(name: String): String {
