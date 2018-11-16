4f3d5def45a245d0c98a0737b8c5fbe87c6526f0
Christopher Salvador Márquez Alvarez <a01225198@itesm.mx>
2018-11-15 21:14:25 -0600

    Add Field

--- src/main/kotlin/Services/ParagraphParser.kt
+++ src/main/kotlin/Services/ParagraphParser.kt
@@ -2,6 +2,7 @@ package Services
 
 import Interfaces.ICodeGenerator
 import Interfaces.IParagraphParser
+import java.lang.IllegalStateException
 
 class ParagraphParser(
     private val codeGen: ICodeGenerator
@@ -9,15 +10,18 @@ class ParagraphParser(
 
     private var className = ""
 
+    private val value = "(\"\\w\"|[0-9]+(.[0-9]+)?)"
+    private val fields = mutableListOf<String>()
+
     override fun parseParagraph(paragraph: String): String {
         val sentences = paragraph.split("\\.")
-        for (sentence in sentences) matchPattern(sentence)
+        for (sentence in sentences) matchPattern(sentence.trim())
         return codeGen.buildObject(className)
     }
 
-
     private fun matchPattern(sentence: String) = when {
-        "There is a [a-zA-Z ]+".toRegex().containsMatchIn(sentence) -> createObject(sentence)
+        "There is a \\w".toRegex().containsMatchIn(sentence) -> createObject(sentence)
+        "It has \\w of $value".toRegex().matches(sentence) -> addField(sentence)
         else -> throw IllegalArgumentException("No match found")
     }
 
@@ -27,6 +31,17 @@ class ParagraphParser(
         var className = ""
         for (i in 3..splitted.size) className += splitted[i]
         this.className = className
+    }
+
+    private fun addField(sentence: String): String {
+        val splitted = sentence.split(" ")
+        if (splitted.size != 5) throw IllegalArgumentException("Add field malformed: $sentence")
+        val key = splitted[2]
+        val value = splitted[4]
+        if (fields.find { it == key } != null) throw IllegalStateException("Duplicate field: $key")
+        fields.add(key)
+        codeGen.genField(key, value)
+        return "$key,$value"
     }
 
 }
--- src/test/kotlin/Services/ParagraphParserTest.kt
+++ src/test/kotlin/Services/ParagraphParserTest.kt
@@ -1,12 +1,20 @@
 package Services
 
-import Interfaces.IParagraphParser
+import Interfaces.ICodeGenerator
+import Models.Expression
 import org.junit.Assert
 import org.junit.jupiter.api.Test
+import kotlin.reflect.jvm.isAccessible
 
 internal class ParagraphParserTest {
 
-    //private val parser: IParagraphParser = ParagraphParser()
+    private val parser = ParagraphParser(object : ICodeGenerator {
+        override fun genField(key: String, value: String): ICodeGenerator = this
+        override fun genStatement(expression: Expression, varName: String?): ICodeGenerator = this
+        override fun genMethod(name: String, args: List<String>): ICodeGenerator = this
+        override fun buildObject(name: String): String = ""
+
+    })
 
     @Test
     fun parseParagraph() {
@@ -21,5 +29,19 @@ internal class ParagraphParserTest {
 
     @Test
     fun matchPattern() {
+    }
+
+    @Test
+    fun addField() {
+        val test1 = "It has age of 5"
+        val test2 = "It has name of \"Miguel\""
+        val addFieldMethod = ParagraphParser::class.members.find { it.name == "addField" }
+        addFieldMethod?.let {
+            it.isAccessible = true
+            val field1 = it.call(parser, test1) as String
+            val field2 = it.call(parser, test2) as String
+            Assert.assertEquals("age,5", field1)
+            Assert.assertEquals("name,\"Miguel\"", field2)
+        }
     }
 }
