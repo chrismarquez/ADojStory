456f78b48d7a4af06fe06ea7aef621000d1767d4
christopher <christophersmarquez@outlook.com>
2018-11-20 21:14:14 -0600

    Added Beautifier

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,8 @@
-import kotlinx.coroutines.*
+import Services.Beautifier
+
 
 fun main() {
     println("Hello World!")
+    val js = Beautifier.beautify("var a=1;b=2;var user={name:\n\"Andrew\"};")
+    println(js)
 }
